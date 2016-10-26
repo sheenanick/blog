68e3894310a76137cac3ca58c1103ade11ec81b4
Shelby Clayton and Sheena Nickerson <shelby@>
2016-10-26 10:40:34 -0700

    add update functionality

--- app/components/admin-tile.js
+++ app/components/admin-tile.js
@@ -6,6 +6,9 @@ export default Ember.Component.extend({
       if (confirm('Are you sure you want to delete this post?')) {
         this.sendAction('destroyPost', post);
       }
+    },
+    update(post, params) {
+      this.sendAction('update', post, params);
     }
   }
 });
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -17,6 +17,16 @@ export default Ember.Route.extend({
     destroyPost(post) {
       post.destroyRecord();
       this.transitionTo('admin');
+    },
+
+    update(post, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          post.set(key, params[key]);
+        }
+      });
+      post.save();
+      this.transitionTo('admin');
     }
   }
 });
