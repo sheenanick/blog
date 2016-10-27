a6e37bfa4e4fe55886d6502d617f561d22a9bbfd
Shelby Clayton and Sheena Nickerson <shelby@>
2016-10-27 14:06:55 -0700

    add computed component to sort comments

--- app/components/post-detail.js
+++ app/components/post-detail.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['date:desc'],
+  sortedComments: Ember.computed.sort('post.comments', 'sortBy'),
   actions: {
     saveComment(params) {
       this.sendAction('saveComment', params);
